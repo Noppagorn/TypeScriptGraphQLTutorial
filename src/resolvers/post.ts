5846ea755758b3378a78db614c6b95deef1a9fa9
NoppaGorn <tongnop2540@gmail.com>
2021-05-08 02:12:25 +0700

    can get post and create post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver{
@@ -9,5 +9,22 @@ export class PostResolver{
         @Ctx() {em} : MyContext
     ) : Promise<Post[]> {
         return em.find(Post,{});
+    }
+
+    @Query(() => Post,{nullable : true}) // this is graphQl type
+    post(
+        @Arg('id') id: number,
+        @Ctx() {em} : MyContext
+    ) : Promise<Post | null> { // this is type script type
+        return em.findOne(Post,{ id });
+    }
+    @Mutation(() => Post) // Mutation for edit insert update data
+    async createPost(
+        @Arg('title') title : string,
+        @Ctx() {em} : MyContext
+    ) : Promise<Post> { // this is type script type
+        const post = em.create(Post,{title})
+        await em.persistAndFlush(post)
+        return post
     }
 }
